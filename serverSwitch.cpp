2eb6ad95cd13a67d2934c5a8934ae4645fdc02f9
jgillen1 <jeffreygillen06@gmail.com>
2016-04-25 03:19:26 -0400

    Update serverSwitch.cpp

--- serverSwitch.cpp
+++ serverSwitch.cpp
@@ -11,7 +11,7 @@ int status = IDLE;
 void simSwitch(){
 //initialize
 deviceNode.generateKeysRSA();
-	while (;;) {
+	while (true) {
 
 
 		switch (status)
@@ -105,7 +105,7 @@ void data_trans() {
 
 
 
-	while (;;) {
+	while (true) {
 
 		char* packet = listen(PORT);
 
